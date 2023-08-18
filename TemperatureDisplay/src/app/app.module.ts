3a8b5ebaa6bbe823e68482f0bab3f1623060429d
unknown <MightyDevKnight>
2023-08-18 14:20:34 +0200

    feat: add temp input component

--- TemperatureDisplay/src/app/app.module.ts
+++ TemperatureDisplay/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemperatureInputComponent } from './temperature-input/temperature-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemperatureInputComponent
   ],
   imports: [
     BrowserModule
